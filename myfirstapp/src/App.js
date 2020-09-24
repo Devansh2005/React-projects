fe0909b480efb171131caa40a20c348130d757cb
livrbecca <69110329+livrbecca@users.noreply.github.com>
2020-09-24 20:57:41 +0100

    bookshop and first app

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="../click4.png" className="App-logo" alt="logo" />
+        <h1 className="heading" style={{color: "#FFC0C9"}}>
+          Hi. How are you?
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
