92f0379d1cb28fe19ed654d01a735d2280b06e3d
livrbecca <69110329+livrbecca@users.noreply.github.com>
2020-09-29 23:03:08 +0100

    first push on react exercise 1 bookshop

--- bookshop/src/App.js
+++ bookshop/src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './book.css';
+import './Product';
+import Product from './Product';
+
+const products = [
+  {
+   name: "Snow White",
+   price: "£3.99",
+   description: "A classic"
+  },
+  {
+    name: "Cinderella",
+    price: "£5.99",
+    description: "Another classic"
+
+  }
+]
 
 function App() {
+const name = 'Beyonce';
+function Greeting(user) {
+    if (user) {
+        return <h1>{name}'s Reading List</h1>;
+    }
+    return <h1>Just a Reading List</h1>;
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 className="heading">Welcome to the Book Library</h1>
+      <img src="../emojibook.png" className="App-logo" alt="logo" />
+        <h2 className='slogan'>
+          Where Books Come to Life
+        </h2>
       </header>
+      {products.map(product => (
+        <Product
+        name={product.name}
+        price={product.price}
+        description={product.description}
+        />
+      ))}
+    
+
+      {Greeting(name)}
     </div>
   );
 }
+
 
 export default App;
